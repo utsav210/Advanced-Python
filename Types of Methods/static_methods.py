f23fdcc6120e5730f8043c274a1d4f77a48b90b0
Utsav <utsavgandhi273@gmail.com>
2024-07-21 18:29:04 +0530

    @staticmethod used

---
+++ Types of Methods/static_methods.py
@@ -0,0 +1,22 @@
+'''
+Challenge/Task:
+
+Define a class MathUtils with a static method add_numbers that takes two numbers and returns their sum.
+Call the static method without creating an instance of the class.
+'''
+
+class MathUtils:
+    @staticmethod
+    def add_numbers(a, b):
+        """Static method to add two numbers"""
+        return a + b
+    
+def main():
+    a = int(input("Enter the input number A: "))
+    b = int(input("Enter the input number B: "))
+    """without creating instance of the class"""
+    result = MathUtils.add_numbers(a,b)
+    print(f"Sum of two numbers: {result}")
+
+if __name__ =="__main__":
+    main()
